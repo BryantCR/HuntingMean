9c06fd8bcc2112e961981558e4840413c7b6715b
BryantCR <bryancvargas20@gmail.com>
2021-11-16 16:30:38 -0600

    ok

--- HuntingMean.js
+++ HuntingMean.js
@@ -1,19 +1,22 @@
-let button = $('#action');
-let user = $('#user').val();
-let usern = document.getElementById('user').value;
-console.log(user, "Esto es USER");
-//console.log(usern, "Esto es USERN");
-// https://api.github.com/users/${User}
 
-$.get(`https://api.github.com/users/${user}`, displayName)
-// Notice that displayName is a function that takes in 1 parameter (this is the data that comes back from the API)
-function displayName(data) {
-    console.log(data);
+function prueba(event){
+
+    event.preventDefault();
+
+    let user = $('#user').val();
+    $.get(`https://api.github.com/users/${user}`, displayName)
+    console.log('Working correctly')
+    function displayName(data) {
+        console.log(data.login);
+        document.querySelector('.results').innerHTML =`
+        <h1>User: ${data.login}</h1>
+        <h2>Name: ${data.name}</h2>
+        <img style="width: 200px;" src = "${data.avatar_url}">
+        `;
+        
+    }
+
 }
 
-
-
-
-
-
+$( '.gitForm' ).on( 'submit', prueba )
 
