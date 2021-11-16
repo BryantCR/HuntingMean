efb07f8ccdf44a2b589e20e0b48ec96fca8d9aa8
BryantCR <bryancvargas20@gmail.com>
2021-11-16 15:08:33 -0600

    Ok

---
+++ HuntingMean.js
@@ -0,0 +1,15 @@
+$.get("https://api.github.com/users/(... user name)", displayName)
+// Notice that displayName is a function that takes in 1 parameter (this is the data that comes back from the API)
+function displayName(data) {
+    console.log(data);
+}
+
+$(document).ready(function(){
+
+    
+
+});
+
+
+
+
